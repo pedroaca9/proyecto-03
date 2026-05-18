547becb4d09190f450551d728c961c2627de14d3
Flores Pedro <pedroaca84@gmail.com>
2026-05-18 10:52:19 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+        <h1> HOLA</h1>
+      <div> 
+
+          <nav className="">
+            
+              <a className="" aria-current="page" href="#">Home</a>
+              <a className="" href="#">Features</a>
+              <a className="" href="#">Pricing</a>
+              <a className="" href="#" >contact</a>
+                     
+          </nav>
+        
+      
+       </div>
+    </>
+  )
+
+}
+export default App()
